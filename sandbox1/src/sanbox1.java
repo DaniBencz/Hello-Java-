eaf7e91fda14d8a5bee9ef359ebee8ec6920a5db
Daniel Bencz <daniel.bencz@gmail.com>
2019-06-27 11:02:44 +0200

    initial commit

---
+++ sandbox1/src/sanbox1.java
@@ -0,0 +1,13 @@
+public class sanbox1 {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        do1();
+
+    }
+
+    public static void do1(){
+        int myNumber = 1000;
+        myNumber = 2000;
+        System.out.print(myNumber);
+    }
+}
---
+++ sandbox1/src/sandbox2.java
@@ -0,0 +1,9 @@
+public class sandbox2 {
+    public static void main(String[] args) {
+        //variables
+
+        int myNumber = 1000;
+        System.out.print(myNumber);
+
+    }
+}
